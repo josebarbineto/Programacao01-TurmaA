7226722bab4e67dcf88f8191506da249da112d04
aluno <aluno@lab01-12>
2026-03-10 11:11:28 -0300

    MPA

--- Jose/src/main/java/ifc/jose/SitemaIfc.java
+++ Jose/src/main/java/ifc/jose/SitemaIfc.java
@@ -3,14 +3,16 @@ package ifc.jose;
 public class SitemaIfc {
 
     public static void main(String[] args) {
-        Estudante jose = new Estudante();
+        Estudante camada = new Estudante();
         Estudante joao = new Estudante();
         Estudante geovanni = new Estudante();
 
-        jose.setNome("José");
+        camada.setNome("José");
         joao.setNome("João");
         geovanni.setNome("Geovanni");
-        
-        System.out.println("");
+
+        System.out.println(camada.getNome());
+        System.out.println(joao.getNome());
+        System.out.println(geovanni.getNome());
     }
 }

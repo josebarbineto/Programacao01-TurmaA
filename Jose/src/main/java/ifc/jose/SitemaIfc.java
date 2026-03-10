eed6ccda371b132a5954213c0b07a9a7ca97c403
aluno <aluno@lab01-12>
2026-03-10 11:04:59 -0300

    MPA

--- Jose/src/main/java/ifc/jose/SitemaIfc.java
+++ Jose/src/main/java/ifc/jose/SitemaIfc.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ifc.jose;
 
-/**
- *
- * @author aluno
- */
 public class SitemaIfc {
-    
+
+    public static void main(String[] args) {
+        Estudante jose = new Estudante();
+        Estudante joao = new Estudante();
+        Estudante geovanni = new Estudante();
+    }
 }
